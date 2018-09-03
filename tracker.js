34be3364c04d57d562eaee250a32a88c3d540ed8
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2018-09-03 14:05:13 -0700

    removed commented

--- tracker.js
+++ tracker.js
@@ -386,8 +386,6 @@ function calculateInvestmentPerformanceOverTime2(time_interval) {
 			// console.log("state:", state)
 			// console.log("stock_prices", stock_prices)
 			balance = calculatePortfolioBalance(state, stock_prices, current_date_str)
-			console.log("portfolio balance: ", balance)
-			console.log("--------------")	
 			portfolio_balance.push(balance)
 			current_date2.setDate(current_date2.getDate() + time_interval)
 					
@@ -486,14 +484,6 @@ function calculatePortfolioBalance(portfolio_state, stock_prices, date) {
 
 }
 
-// state = calcualtePortfolioStateOnDate('2018-09-03')
-// console.log("portfolio state on date", state)
-// getPortfolioClosingPriceData(state).then((stock_prices) => {
-// 	balance = calculatePortfolioBalance(state, stock_prices, '2018-08-23')
-// 	console.log("portfolio balance: ", balance)
-// })
-
-
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array)
@@ -531,33 +521,6 @@ function workingDaysBetweenDates(startDate, endDate) {
     // Validate input
     if (endDate < startDate)
         return 0;
-    
-    // // Calculate days between dates
-    // var millisecondsPerDay = 86400 * 1000; // Day in milliseconds
-    // startDate.setHours(0,0,0,1);  // Start just after midnight
-    // endDate.setHours(23,59,59,999);  // End just before midnight
-    // var diff = endDate - startDate;  // Milliseconds between datetime objects    
-    // var days = Math.ceil(diff / millisecondsPerDay);
-    
-    // // Subtract two weekend days for every week in between
-    // var weeks = Math.floor(days / 7);
-    // days = days - (weeks * 2);
-
-    // // Handle special cases
-    // var startDay = startDate.getDay();
-    // var endDay = endDate.getDay();
-    
-    // // Remove weekend not previously removed.   
-    // if (startDay - endDay > 1)         
-    //     days = days - 2;      
-    
-    // // Remove start day if span starts on Sunday but ends before Saturday
-    // if (startDay == 0 && endDay != 6)
-    //     days = days - 1  
-            
-    // // Remove end day if span ends on Saturday but starts after Sunday
-    // if (endDay == 6 && startDay != 0)
-    //     days = days - 1  
     
     let days = 0
     let holidays = 0
